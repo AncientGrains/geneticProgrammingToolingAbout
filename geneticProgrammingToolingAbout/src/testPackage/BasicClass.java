f400d918e902c227ba4557a7f8df6dae568b9320
jack <jack.gammon@equinox.co.nz>
2015-12-11 12:29:44 +1300

    trying to get automation going...

---
+++ geneticProgrammingToolingAbout/src/testPackage/BasicClass.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+public class BasicClass {
+
+	public BasicClass(){
+		System.out.println("HELLO WORLD!");
+	}
+	
+	
+	public static void main(String[] args) {
+		new BasicClass();
+	}
+
+}
