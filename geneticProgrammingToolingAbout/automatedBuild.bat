1cc4fa1fd2a88430f5ee126195a3d93fb55cfa13
jack <jack.gammon@equinox.co.nz>
2015-12-11 12:52:56 +1300

    add automated build script

---
+++ geneticProgrammingToolingAbout/automatedBuild.bat
@@ -0,0 +1,4 @@
+REM "C:\Program Files\Java\jdk1.8.0_66\bin\javac" -d bin -sourcepath src src\testPackage\BasicClass.java
+REM java -cp bin; testPackage.BasicClass
+REM javac -d geneticProgrammingToolingAbout\bin -sourcepath src geneticProgrammingToolingAbout\src\testPackage\BasicClass.java
+REM java -cp bin; testPackage.BasicClass
